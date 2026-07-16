a7c9c8d4d573ee5bf04c9ead66992cd9c4e27abd
Mathod <felix.mathias95@gmail.com>
2026-07-16 17:11:56 +0200

    + home-manager

--- flake.nix
+++ flake.nix
@@ -1,20 +1,28 @@
 {
-
   description = "Mathod NixOS";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     nixos-wsl.url = "github:nix-community/NixOS-WSL/main";
   };
 
-  outputs = { self, nixpkgs, nixos-wsl, ... }: {
+  outputs = { self, nixpkgs, home-manager, nixos-wsl, ... }: {
     nixosConfigurations = {
       wsl = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
           nixos-wsl.nixosModules.default
-	  ./configuration.nix
+	        ./configuration.nix
+          home-manager.nixosModules.home-manager
           {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.extraSpecialArgs = { inherit inputs; };
+            #home-manager.users.mathod = ./modules/default.nix;
             system.stateVersion = "26.05";
             wsl.enable = true;
           }
